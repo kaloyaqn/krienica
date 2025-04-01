abdcf154a18cbb2ebd9885b0893f036f5cb45b59
kaloyaqn <kallnoob11@gmail.com>
2025-04-01 19:50:31 +0300

    fix?

--- app/admin/users/page.js
+++ app/admin/users/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { database } from '@/lib/firebase';
+import { database } from "../../../lib/firebase"
 import { ref, onValue, set } from 'firebase/database';
 
 export default function UserManagement() {
